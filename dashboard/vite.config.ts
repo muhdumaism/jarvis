e622c691d3aa2b87d39c64f6354e949633af114b
Muhammed Umais <muhdumaism@gmail.com>
2026-08-14 15:16:38 +0530

    Enable local HTTPS using basicSsl plugin in Vite config

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    basicSsl()
+  ],
   server: {
     host: true, // Listen on all network interfaces to allow phone connections
     proxy: {
